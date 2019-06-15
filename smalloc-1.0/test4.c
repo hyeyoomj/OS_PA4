8dcf9511c4bf2561542c17dc2898b52ff47af97f
hyeyoomj <hyeyoomj@gmail.com>
2019-06-15 21:18:05 +0900

    .

--- smalloc-1.0/test4.c
+++ smalloc-1.0/test4.c
@@ -9,30 +9,26 @@ main()
 	print_sm_containers() ;
 
 	p1 = smalloc(1000) ; 
-	printf("smalloc(2000)\n") ; 
+	printf("smalloc(1000)\n") ; 
 	print_sm_containers() ;
 
 	p2 = smalloc(1000) ; 
-	printf("smalloc(2500)\n") ; 
-	print_sm_containers() ;
-
-	sfree(p1) ; 
-	printf("sfree(%p)\n", p1) ; 
+	printf("smalloc(1000)\n") ; 
 	print_sm_containers() ;
 
 	p3 = smalloc(2500) ; 
-	printf("smalloc(1000)\n") ; 
+	printf("smalloc(2500)\n") ; 
 	print_sm_containers() ;
 
 	p4 = smalloc(1000) ; 
 	printf("smalloc(1000)\n") ; 
 	print_sm_containers() ;
 
-        sfree(p3);
-	printf("sfree(%p)\n",p3);
+        sfree(p4);
+	printf("sfree(%p)\n",p4);
 	print_sm_containers();
 
-        p2 = smalloc(500) ; 
-	printf("smalloc(2500)\n") ; 
+        p4 = smalloc(500) ; 
+	printf("smalloc(500)\n") ; 
 	print_sm_containers() ;
 }
